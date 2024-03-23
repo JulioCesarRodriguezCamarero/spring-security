ff07a046a375446f636da04f57fc0cf11e209fd2
Julius Kaesar <j3dianas@gmail.com>
2024-03-23 18:21:12 +0100

    commit 4 fin configuración ok.

--- src/main/java/or/grisu/springwebsecurity/config/SecurityConfig.java
+++ src/main/java/or/grisu/springwebsecurity/config/SecurityConfig.java
@@ -2,10 +2,14 @@ package or.grisu.springwebsecurity.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.session.SessionRegistry;
+import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 @Configuration
 @EnableWebSecurity
@@ -22,11 +26,39 @@ public class SecurityConfig {
 
 
                 })
-                .formLogin(AbstractAuthenticationFilterConfigurer::permitAll)
+                .formLogin(auth -> {
+                    auth
+                            .successHandler(successHandler())// Url redirección de sesión
+                            .permitAll();
+                })
+                .sessionManagement(ses -> {
+                    ses.sessionCreationPolicy(SessionCreationPolicy.ALWAYS)// ALWAYS - IF_REQUIRED - NEVER - STATELESS
+                            .invalidSessionUrl("/login")
+                            .maximumSessions(1)
+                            .expiredUrl("/login")
+                            .sessionRegistry(sessionRegistry());
 
+                    ses.sessionFixation()
+
+                            .migrateSession(); //La sesión crea un Id de sesión para proteger de ataques
+                    // migrateSession - newSession - none
+
+                })
+//                .httpBasic(Customizer.withDefaults())                 // Autentificación básica no recomendada
                 .build();
     }
 
+
+    public AuthenticationSuccessHandler successHandler() {
+        return ((request, response, authentication) -> {
+
+            response.sendRedirect("/v1/session");
+        });
+    }
+    @Bean
+    public SessionRegistry sessionRegistry() {
+        return new SessionRegistryImpl();
+    }
 
 }
 
--- src/main/java/or/grisu/springwebsecurity/controller/ControladorPersonal.java
+++ src/main/java/or/grisu/springwebsecurity/controller/ControladorPersonal.java
@@ -1,13 +1,24 @@
 package or.grisu.springwebsecurity.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.session.SessionInformation;
+import org.springframework.security.core.session.SessionRegistry;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("v1")
 public class ControladorPersonal {
 
+    @Autowired
+    private SessionRegistry sessionRegistry;
 
     @GetMapping("/index")
     public String index(){
@@ -16,6 +27,30 @@ public class ControladorPersonal {
 
     @GetMapping("/index2")
     public String index2(){
+
         return "Hola caracola sin seguridad";
+    }
+    @GetMapping("/session")
+    public ResponseEntity<?> getDetailsSession(){
+
+        String sessionId = "";
+        User userObject = null;
+
+        List<Object> sessions= sessionRegistry.getAllPrincipals();
+        for(Object session: sessions){
+            if (session instanceof User){
+                userObject =(User) session;
+            }
+          List<SessionInformation> sessionInformation =  sessionRegistry.getAllSessions(session,false);
+            for(SessionInformation sessionInfo: sessionInformation){
+                sessionId = sessionInfo.getSessionId();
+            }
+        }
+        Map<String,Object> response = new HashMap<>();
+        response.put("response", "La madre que me parió");
+        response.put("sessionId", sessionId);
+        response.put("Usuario", userObject);
+
+        return  ResponseEntity.ok(response);
     }
 }
